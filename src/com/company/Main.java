d57c8d7d22e6790307abe8d458211cad89563bba
Łukasz Waręcki <glenski2be7@gmail.com>
2018-11-14 21:50:33 +0100

    Windshield Wipers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        boolean isRaining=true; // true or false
+    // Write an if statement below that prints out "Windshield wipers on." when isRaining is true.
+        if (isRaining){
+            System.out.println("Windshield wipers on.");
+        } else {
+            System.out.println("Windshield wipers off.");
+        }
     }
 }
