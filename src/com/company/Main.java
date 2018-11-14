9293dc96675674b7953349f963249d8e893cd06b
Łukasz Waręcki <glenski2be7@gmail.com>
2018-11-14 22:00:51 +0100

    else-if

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        boolean isRaining=true; // true or false
-    // Write an if statement below that prints out "Windshield wipers on." when isRaining is true.
-        if (isRaining){
-            System.out.println("Windshield wipers on.");
+        boolean isLightGreen=true; // true or false
+        boolean isLightYellow=false; // true or false
+
+        if (isLightGreen){
+            System.out.println("Drive!");
+        } else if (isLightYellow){
+            System.out.println("Slow down.");
         } else {
-            System.out.println("Windshield wipers off.");
+            System.out.println("Stop.");
         }
     }
 }
